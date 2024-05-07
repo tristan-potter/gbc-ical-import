c6c11fc605a1eef62a829d223d4f76ab368c6655
Tristan Potter <4521510+DrTeePot@users.noreply.github.com>
2024-05-06 23:12:45 -0400

    Use a more readable course description, and update instructor regex to be less specific

--- src/course.rb
+++ src/course.rb
@@ -10,7 +10,7 @@ class Course
   SCHEDULE_REGEX = /^(?<type>\w+)\t(?<time>#{TIME_RANGE_REGEX})\t(?<days>\w)\t(?<location>.+)\t(?<range>#{DATE_RANGE_REGEX})\t(?<schedule>\w+)\t(?<instructor>.+).*$/
   CAMPUS_REGEX = /^Campus:\s+(?<campus>.*)$/
   CRN_REGEX = /^CRN:\s+(?<crn>\w+)$/
-  INSTRUCTOR_REGEX = /^Assigned Instructor:\s+(?<instructor>[\w\s]+)$/
+  INSTRUCTOR_REGEX = /^Assigned Instructor:\s+(?<instructor>.+)$/
   GRADE_MODE_REGEX = /^Grade Mode:\s+(?<grade_mode>.*)$/
   CREDITS_REGEX = /^Credits:\s+(?<credits>\d+\.\d+)$/
   LEVEL_REGEX = /^Level:\s+(?<level>.*)$/
@@ -25,7 +25,7 @@ class Course
     end
   end
 
-  attr_reader :course_str, :schedules, :title, :course_code, :campus, :crn, :grade_mode
+  attr_reader :course_str, :schedules, :title, :course_code, :campus, :crn, :grade_mode, :credits, :level, :status
 
   def initialize(course_str)
     @course_str = course_str
@@ -37,6 +37,9 @@ class Course
     @campus = @course_str.match(CAMPUS_REGEX)[:campus]
     @crn = @course_str.match(CRN_REGEX)[:crn]
     @grade_mode = @course_str.match(GRADE_MODE_REGEX)[:grade_mode]
+    @credits = @course_str.match(CREDITS_REGEX)[:credits]
+    @level = @course_str.match(LEVEL_REGEX)[:level]
+    @status = @course_str.match(STATUS_REGEX)[:status]
   end
 
   def instructor
@@ -51,5 +54,13 @@ class Course
 
   def description
     course_str
+    <<~DESC
+      Assigned Instructor: #{instructor}
+      Grade Mode: #{grade_mode}
+      Credits: #{credits}
+      Campus: #{campus}
+      Status: #{status}
+      CRN: #{crn}
+    DESC
   end
 end
